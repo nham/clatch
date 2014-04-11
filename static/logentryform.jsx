a529b32ab4f6bd295befd4c5510d4f81633599dc
nham <hamann.nick@gmail.com>
2014-04-11 11:58:46 -0400

    use defaultValue in LogForm render()

--- static/logentryform.jsx
+++ static/logentryform.jsx
@@ -4,17 +4,16 @@
 
 var LogForm = React.createClass({
   handleSubmit: function() {
-      var body = this.refs.body.getDOMNode().value;
-      var tags = this.refs.tags.getDOMNode().value;
-      this.props.onFormSubmit({body: body, tags: tags});
+    var body = this.refs.body.getDOMNode().value;
+    var tags = this.refs.tags.getDOMNode().value;
+    this.props.onFormSubmit({body: body, tags: tags});
 
-      return false; // prevents default action of the browser
+    return false; // prevents default action of the browser
   },
 
   render: function() {
     var body = "";
     var tags = "";
-    console.log(this.props.entry);
     if (this.props.entry !== undefined) {
         body = this.props.entry.body;
         tags = this.props.entry.tags.map(function(tag) { return "#"+tag; }).join(', ');
@@ -33,9 +32,9 @@ var LogForm = React.createClass({
         <form onSubmit={this.handleSubmit}>
           <dl>
             <dt>Body:</dt>
-            <dd><textarea rows="20" cols="80" ref="body">{body}</textarea></dd>
+            <dd><textarea rows="20" cols="80" defaultValue={body} ref="body"></textarea></dd>
             <dt>Tags:</dt>
-            <dd><input type="text" size="50" value={tags} ref="tags" /></dd>
+            <dd><input type="text" size="50" defaultValue={tags} ref="tags" /></dd>
             <dd><input type="submit" value="Submit" /></dd>
           </dl>
         </form>
